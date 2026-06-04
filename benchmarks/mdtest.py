a8df2b151b1df06f3b52cb4a71593bafd604050d
heringpm <heringpm@gmail.com>
2026-06-04 13:12:37 -0700

    Update mdtest.py

--- benchmarks/mdtest.py
+++ benchmarks/mdtest.py
@@ -69,13 +69,12 @@ class MDTestBenchmark:
 
 		with open(f"{self.log_path}/mdtest/{self.runid_base}/{self.fname}.mdtest.log", "w") as log_file:
 
-			print(cmd)
-			#process = subprocess.run(
-			#    ["bash", "-c", cmd],
-			#    stdin=subprocess.DEVNULL,
-			#    stdout=log_file,
-			#    stderr=log_file
-			#)
+			process = subprocess.run(
+			    ["bash", "-c", cmd],
+			    stdin=subprocess.DEVNULL,
+			    stdout=log_file,
+			    stderr=log_file
+			)
 
 
 	def stop(self):
